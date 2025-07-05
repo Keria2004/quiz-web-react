2ff9f47d0ea691f9079e38324c9a0a2aecc9c571
Keria2004 <sonhuynhvo12345@gmail.com>
2025-07-05 10:41:38 +0700

    video 18

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -6,10 +6,22 @@ class MyComponent extends React.Component {
     address: "TPHCM",
     age: 21,
   };
+
+  hanleClick(event) {
+    //console.log("Click me");
+    console.log("My name is", this.state.name); // => lỗi
+  }
+
+  handleOnMouseOver(event) {
+    //console.log(event.pageX);
+  }
+
   render() {
     return (
       <div>
         My name is {this.state.name} and I from {this.state.address}
+        <button onClick={this.hanleClick}>Click me</button>
+        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
       </div>
     );
   }
